a889f67d88deec96aaafa2924a32560512f320ad
dario-rodrigues-controlp <61985962+dario-rodrigues-controlp@users.noreply.github.com>
2020-03-10 15:00:52 -0300

    Add files via upload

--- www/gedi800.js
+++ www/gedi800.js
@@ -1,11 +1,5 @@
-var exec = require( 'cordova/exec' );
+var exec = require('cordova/exec');
 
-exports.printText = function ( arg0, success, error ) 
-{
-    exec( success, error, 'gedi800', 'printText', [ arg0 ] );
-};
-
-exports.printBarcode = function ( arg0, success, error ) 
-{
-    exec( success, error, 'gedi800', 'printBarcode', [ arg0 ] );
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'gedi800', 'coolMethod', [arg0]);
 };
